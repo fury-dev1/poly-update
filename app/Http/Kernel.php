279b9bb3b0d0358bb40c5be9947913e541f30726
Tony Starikov <tony.starikov.1992@gmail.com>
2023-06-02 16:24:10 +0300

    Created controllers, views, middlewares and complete admin panel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,8 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\CheckIsAdmin::class,
+        \App\Http\Middleware\SetLocale::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
@@ -53,6 +55,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
+        'is_admin' => \App\Http\Middleware\CheckIsAdmin::class,
+        'set_locale' => \App\Http\Middleware\SetLocale::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
